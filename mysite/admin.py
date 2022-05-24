28c0cf170b1ef6b769fd8ca2a6de6a9f69bc1137
JOHN BATISTA <ebukaiikenwa0902@gmail.com>
2022-05-24 03:03:53 -1100

    Still Learning

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from mysite.models import PreviousProjects
 
 # Register your models here.
+admin.site.register(PreviousProjects)
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class PreviousProjects(models.Model):
+    name = models.CharField(max_length=40)
+    shortDescription = models.TextField(max_length=100)
+    largeDescription = models.TextField(max_length=4000)
+    appLink = models.TextField(max_length=50)
